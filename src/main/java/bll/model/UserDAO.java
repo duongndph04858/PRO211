2f5a316caab682822d8e2a63a5a2bcef98611a18
duongndph04858 <duongndph04858@fpt.edu.vn>
2018-11-09 14:57:55 +0700

    thêm class Base

---
+++ src/main/java/bll/model/UserDAO.java
@@ -0,0 +1,9 @@
+package bll.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDAO extends Base {
+
+}
